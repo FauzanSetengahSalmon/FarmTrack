21a14d2f90602b8ef989ae10039083401d7cec7c
FauzanSetengahSalmon <ahmadfauzan201006@gmail.com>
2026-06-16 09:40:03 +0700

    Memperbagus isi tema dan warnanya

--- app/src/main/java/com/fauzan0022/farmtrack/ui/theme/Color.kt
+++ app/src/main/java/com/fauzan0022/farmtrack/ui/theme/Color.kt
@@ -9,3 +9,21 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val EmeraldPrimary = Color(0xFF1B6A4F)
+val EmeraldSecondary = Color(0xFF348E6E)
+val EmeraldTertiary = Color(0xFF4C5D56)
+
+val FarmBackground = Color(0xFFF5F7F2)
+val FarmCardBorder = Color(0xFFE2E8F0)
+val FarmTextMain = Color(0xFF1E293B)
+val FarmTextSec = Color(0xFF64748B)
+
+val AlertPendingBg = Color(0xFFFFFBEB)
+val AlertPendingBorder = Color(0xFFFEF3C7)
+val AlertPendingText = Color(0xFF92400E)
+
+val DarkPrimary = Color(0xFF86EFAC)
+val DarkSecondary = Color(0xFF4ADE80)
+val DarkBackground = Color(0xFF0F172A)
+val DarkSurface = Color(0xFF1E293B)
